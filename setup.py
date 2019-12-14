1ea16a9e3f71613234a40a9d7415a96edca7ca65
Thorben Jensen <jensen.thorben@gmail.com>
2019-12-14 13:24:55 +0100

    adding environment files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from glob import glob
+from setuptools import setup, find_packages
+
+setup(
+    name="tune-demo",
+    version="0.1.0",
+    # about
+    author="Thorben Jensen",
+    author_email="jensen.thorben@gmail.com",
+    license="MIT",
+    url="https://github.com/thorbenJensen/tune-demo",
+    # source
+    packages=find_packages(),
+    scripts=glob("bin/*"),
+    # dependencies
+    install_requires=["ray", "scikit-learn"],
+    extras_require={"dev": ["black", "jupyter", "pylama", "rope"],},
+)
