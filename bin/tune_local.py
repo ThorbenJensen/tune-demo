56924036aaf98ae46358718e4f5e08987955193d
Thorben Jensen <jensen.thorben@gmail.com>
2019-12-14 15:35:44 +0100

    refactor: simplify experiment

--- bin/tune_local.py
+++ bin/tune_local.py
@@ -6,7 +6,7 @@ from tune_demo.train import get_iris_data, rf_cv
 
 
 # %%
-def eval_rf(config):
+def eval_model(config):
     X, y = get_iris_data()
     for i in range(3):
         acc = rf_cv(config, X, y)
@@ -15,7 +15,7 @@ def eval_rf(config):
 
 # %%
 analysis = tune.run(
-    eval_rf,
+    eval_model,
     config={
         "max_depth": tune.grid_search([1, 4, 20]),
         "n_estimators": tune.grid_search([10, 100, 1000]),
@@ -25,10 +25,7 @@ analysis = tune.run(
 print("Best config: ", analysis.get_best_config(metric="acc"))
 # config = {'max_depth': 4, 'n_estimators': 1000}
 
-analysis.fetch_trial_dataframes()
-
 # Get a dataframe for analyzing trial results.
-df = analysis.dataframe()
-print(df)
+print(analysis.dataframe())
 
 # %%
