4d58755e88ad3e5e692bab6c2c6c2368403c2531
Thorben Jensen <jensen.thorben@gmail.com>
2019-12-14 22:16:35 +0100

    add 'early stopping' to hyperopt example

--- bin/03_tune_hyperopt_stopping.py
+++ bin/03_tune_hyperopt_stopping.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-# Sequential model-based hyperparameter search with Tree-Structured Parzen Estimator
+# Search with Tree-Structured Parzen Estimator, plus early stopping.
 # See: https://towardsdatascience.com/fast-hyperparameter-tuning-at-scale-d428223b081c
 
 # %%
 from hyperopt import hp
 from ray import tune
+from ray.tune.schedulers import ASHAScheduler
 from ray.tune.suggest.hyperopt import HyperOptSearch
 
 from tune_demo.train import get_iris_data, rf_cv
@@ -13,7 +14,7 @@ from tune_demo.train import get_iris_data, rf_cv
 # %%
 def eval_model(config):
     X, y = get_iris_data()
-    for i in range(3):
+    for i in range(5):
         acc = rf_cv(config, X, y)
         tune.track.log(acc=acc)
 
@@ -24,9 +25,19 @@ space = {
     "n_estimators": hp.uniformint("n_estimators", 10, 1000),
 }
 
-hyperopt_search = HyperOptSearch(space=space, max_concurrent=3, metric="acc", gamma=0.2)
+hyperopt_search = HyperOptSearch(
+    space=space,
+    max_concurrent=4,
+    metric="acc",
+    gamma=0.2,
+)
 
-analysis = tune.run(eval_model, num_samples=30, search_alg=hyperopt_search)
+analysis = tune.run(
+    eval_model,
+    num_samples=50,
+    scheduler=ASHAScheduler(metric="acc", mode="max"),
+    search_alg=hyperopt_search,
+)
 
 print("Best config: ", analysis.get_best_config(metric="acc"))
 # config = {'max_depth': 4, 'n_estimators': 1000}
